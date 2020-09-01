3ed0d1a9c0a68562ab3f511196ec4fd168b485d5
MenekseYuncu <baioretto66@gmail.com>
2020-09-01 23:08:19 +0300

    dosya açmak

--- test4.py
+++ test4.py
@@ -1,7 +1,6 @@
-# w =Yaz - Yazmak için bir dosya açar, yoksa dosyayı oluşturur
-# r = Oku - Varsayılan değer. Okumak için bir dosya açar, dosya yoksa hata
-# a =Ekle - Eklemek için bir dosya açar, yoksa dosyayı oluşturur
+f = open("demofile3.txt", "w")
+f.write("içeriği sildim")
+f.close()
 
-dosya = open("yazilim.txt", "a")
-
-dosya.write("how are you")
+f = open("demofile.txt", "r")
+print(f.readline())
