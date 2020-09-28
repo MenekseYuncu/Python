f614d746c7bbe78e138b5950a7246e402d36aee4
MenekseYuncu <baioretto66@gmail.com>
2020-09-28 22:22:30 +0300

    yapıcı fonk. (init)

--- test5.py
+++ test5.py
@@ -1,3 +1,4 @@
+"""
 class halka:
     halka_sayisi = 5
 
@@ -19,3 +20,30 @@ halka_2 = halka()
 halka_1.halka_at()
 halka_1.halka_at()
 halka_1.kac_halka_kaldı()
+"""
+
+
+class dusman:
+
+    def __init__(self, isim="Dusman", kalan_can=500, saldiri_gucu=70, mermi_sayisi=6):
+        self.isim = isim
+        self.kalan_can = kalan_can
+        self.saldiri_gucu = saldiri_gucu
+        self.mermi_sayisi = mermi_sayisi
+
+    def print(self):
+        print("Ready ----------")
+        print("İsim:", self.isim, "Kalan Can:", self.kalan_can, "Saldırı Gucu:",
+              self.saldiri_gucu, "Mermi Sayısı:", self.mermi_sayisi)
+
+
+dusman1 = dusman("Butterfly", 60, 15, 9)
+dusman2 = dusman("Cat", 90, 100, 7)
+dusman3 = dusman()
+
+dusman1.print()
+print("BUTTERFLY*********")
+dusman2.print()
+print("CAT***********")
+dusman3.print()
+print("DUSMAN*****")
